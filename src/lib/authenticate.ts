87e91dd5eb71d7b3c63f10dfbf7b1d78a18e363f
WildanMukmin <wildanmukmin26@gmail.com>
2025-03-18 16:13:35 +0700

    fixx bug in fetch current user

--- src/lib/authenticate.ts
+++ src/lib/authenticate.ts
@@ -3,12 +3,15 @@ import { prisma } from "./prisma";
 
 export const currentUser = async () => {
   const data = await auth();
-  const user = await prisma.user.findUnique({
-    where: {
-      id: data?.user.id,
-    },
-  });
-  return user;
+  if (data) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: data.user.id,
+      },
+    });
+    return user;
+  }
+  return null;
 };
 
 export const currentUserRole = async () => {
