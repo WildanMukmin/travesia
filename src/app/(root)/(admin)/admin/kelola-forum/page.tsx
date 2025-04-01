500819badb2911f0db2889b11c37a1ea91a04036
WildanMukmin <wildanmukmin26@gmail.com>
2025-04-01 20:51:40 +0700

    add dynamic for forum  management admin

--- src/app/(root)/(admin)/admin/kelola-forum/page.tsx
+++ src/app/(root)/(admin)/admin/kelola-forum/page.tsx
@@ -1,5 +1,7 @@
+import { getAllForum } from "@/actions/forum";
 import AdminKelolaForumPage from "@/components/admin/admin-kelola-forum-page";
 
-export default function Page() {
-  return <AdminKelolaForumPage />;
+export default async function Page() {
+  const forums = await getAllForum();
+  return <AdminKelolaForumPage forums={forums} />;
 }
