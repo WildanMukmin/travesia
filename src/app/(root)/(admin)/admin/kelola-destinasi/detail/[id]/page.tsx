51ace8a7eb4ed2b1f668387a4963a639cff62d68
WildanMukmin <wildanmukmin26@gmail.com>
2025-04-06 23:12:21 +0700

    fix bug

--- src/app/(root)/(admin)/admin/kelola-destinasi/detail/[id]/page.tsx
+++ src/app/(root)/(admin)/admin/kelola-destinasi/detail/[id]/page.tsx
@@ -29,20 +29,7 @@ export default async function Page({ params }: PageProps) {
   return (
     <RoleGate accessRole={Role.ADMIN}>
       <div className="max-w-screen-xl mx-auto px-4">
-        <DestinasiDetailPage
-          userId={data?.owner?.user?.id || ""}
-          id={data?.id || ""}
-          namaDestinasi={data?.namaDestinasi || ""}
-          deskripsi={data?.deskripsi || ""}
-          fasilitas={data?.fasilitas || []}
-          kategoriLokasi={data?.kategoriLokasi || ""}
-          namaOwner={data?.owner?.user?.name || ""}
-          lokasi={data?.alamat || ""}
-          jamOperasional={data?.jamOprasional || ""}
-          harga={data?.harga || 0}
-          role={user?.role}
-          currentUserId={user?.id || ""}
-        />
+        <DestinasiDetailPage destinasiData={data} user={user} />
       </div>
     </RoleGate>
   );
