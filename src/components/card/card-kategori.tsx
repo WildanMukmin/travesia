0e359e0e5f74a134f6283429584cb47b1c95212b
N4thhh <natanaelbungaran@gmail.com>
2025-05-15 21:56:42 +0700

    Delete CardKategori float transition

--- src/components/card/card-kategori.tsx
+++ src/components/card/card-kategori.tsx
@@ -13,7 +13,7 @@ const DestinasiKategoriCard = ({
 }: DestinasiKategoriCardProps) => {
   return (
     <Link href={`/destinasi/kategori/${kategori.split(" ").join("-")}`}>
-      <Card className="rounded-lg overflow-hidden hover:-translate-y-2 transition">
+      <Card className="rounded-lg overflow-hidden">
         <CardHeader className="p-0  overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300">
           <div className="relative h-72 w-64">
             <Image
@@ -22,7 +22,7 @@ const DestinasiKategoriCard = ({
               fill
               className="object-cover brightness-75 hover:brightness-100 transition-all duration-300"
             />
-            <div className="absolute bottom-4 left-4 bg-white bg-opacity-20 hover:bg-opacity-50 backdrop-blur-sm px-4 py-1 rounded-lg hover:text-white">
+            <div className="absolute bottom-4 left-4 bg-black bg-opacity-20 hover:bg-opacity-70 backdrop-blur-sm px-4 py-1 rounded-lg hover:text-white">
               <p className="text-sm font-semibold text-white opacity-70">
                 {kategori}
               </p>
